71d5f50379e86cbbb3b9f1c48303a5f14dd5fce4
gadaharsh <gada.hp@somaiya.edu>
2022-12-04 23:20:41 +0530

    uploadFile for pinning asset

--- contracts/uploadFile.js
+++ contracts/uploadFile.js
@@ -1,12 +1,12 @@
-const pinataApiKey = "INSERT-API-KEY";
-const pinataSecretApiKey = "INSERT-PRIVATE-KEY";
+const pinataApiKey = PINATA_API_KEY;
+const pinataSecretApiKey = PINATA_SECRET_API_KEY;
 const axios = require("axios");
 const fs = require("fs");
 const FormData = require("form-data");
 const pinFileToIPFS = async () => {
   const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
   let data = new FormData();
-  data.append("file", fs.createReadStream("../assets/gradient.png"));
+  data.append("file", fs.createReadStream("../assets/lion.jpg"));
   const res = await axios.post(url, data, {
     maxContentLength: "Infinity", 
     headers: {
